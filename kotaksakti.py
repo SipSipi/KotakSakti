173fea85e9742613fbe8eb78326396d549fa885a
Asif Azamuddin <105183445+SipSipi@users.noreply.github.com>
2022-12-11 15:35:17 +0800

    sql statement correction

--- kotaksakti.py
+++ kotaksakti.py
@@ -10,11 +10,11 @@ def func_sql(sql_statement):
     result = cursor.fetchall()
     return(result)
 
-data1 = func_sql('SELECT customer_id FROM  invoices LEFT JOIN  invoice_lines  ON invoices.id = invoice_lines.invoice_id group by customer_id having (sum(quantity) >= 5)')
+data1 = func_sql('SELECT count(*) FROM ( SELECT SUM(quantity) as quantity_total FROM  invoices i JOIN  invoice_lines j ON i.id = j.invoice_id GROUP BY customer_id HAVING (SUM(quantity)>=5)) as a')
 data2 = func_sql('SELECT name FROM test.customers WHERE NOT EXISTS ( SELECT customer_id from test.invoices WHERE test.invoices.customer_id = test.customers.id)')
 data3 = func_sql('SELECT `description`, `name` FROM invoice_lines i INNER JOIN invoices j ON i.invoice_id = j.id INNER JOIN customers c ON j.customer_id = c.id ORDER BY `description`')
 
-print('Number of customers purchasing more than 5 books : {}'.format(len(data1)))
+print('Number of customers purchasing more than 5 books : {}'.format(data1[0][0]))
 print('\nList of customers who never purchased anything :')
 for row in data2:
     print(row[0])
